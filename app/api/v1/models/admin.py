5e6edb9649c3f1428772150bff9bac9999899f0a
roger254 <rogerbrian.mutua@gmail.com>
2018-10-21 15:23:23 +0300

    [#161365477 created-user-model]

---
+++ app/api/v1/models/admin.py
@@ -0,0 +1,20 @@
+from marshmallow import post_load
+
+from .user import User, UserSchema
+from .user_type import UserType
+
+
+class Admin(User):
+    """Represents the Admin or Owner"""
+
+    def __init__(self, user_name, password):
+        super(Admin, self).__init__(user_name, password, UserType.ADMIN)
+
+    def __repr__(self):
+        return '<User(user_name={self.user_name!r, user_type={self.user_type}))>'.format(self=self)
+
+
+class AdminSchema(UserSchema):
+    @post_load()
+    def make_admin(self, data):
+        return Admin(**data)
