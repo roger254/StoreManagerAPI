a858db76558cf9dacbd45c4fd87bb701f5b21c25
roger254 <rogerbrian.mutua@gmail.com>
2018-10-23 08:28:46 +0300

    [#161365252 added-test-data]

--- app/api/v1/views/products.py
+++ app/api/v1/views/products.py
@@ -1,7 +1,23 @@
 from flask import request, jsonify, make_response
 from flask_classful import FlaskView
 
-products = []
+products = [
+    {
+        'p_name': 'Product 1',
+        'p_price': 23.4,
+        'p_quantity': 45
+    },
+    {
+        'p_name': 'Product 2',
+        'p_price': 45.6,
+        'p_quantity': 56
+    },
+    {
+        'p_name': 'Product 3',
+        'p_price': 54.56,
+        'p_quantity': 56
+    }
+]
 
 
 class ProductView(FlaskView):
@@ -20,4 +36,5 @@ class ProductView(FlaskView):
             return make_response(jsonify(product)), 201
 
     def get(self):
+        # TODO: return valid response if none exists
         return make_response(jsonify(products)), 200
